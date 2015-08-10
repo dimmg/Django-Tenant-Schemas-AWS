6639908ee5729acdd8129920e6fc1d278c3adbc0
dimmg <dumitru.gira@gmail.com>
2015-08-10 14:14:47 +0300

    modified wsgi.py file

--- tenants_aws/wsgi.py
+++ tenants_aws/wsgi.py
@@ -7,8 +7,29 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
+# default wsgi.py file
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tenants_aws.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+
+# alternative wsgi.py file
+
+# import os
+# import sys
+# import site
+
+# site.addsitedir('/home/dimm/Desktop/pythondir/tenants_aws/v_env/local/lib/python2.7/site-packages')
+
+# sys.path.append('/home/dimm/Desktop/pythondir/tenants_aws')
+# sys.path.append('/home/dimm/Desktop/pythondir/tenants_aws/tenants_aws')
+
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'tenants_aws.settings'
+
+# activate_env=os.path.expanduser("/home/dimm/Desktop/pythondir/tenants_aws/v_env/bin/activate_this.py")
+# execfile(activate_env, dict(__file__=activate_env))
+
+# import django.core.handlers.wsgi
+# application = django.core.handlers.wsgi.WSGIHandler()
