ec1d10999bf419f74266fef1276fe561f625f803
dimmg <dumitru.gira@gmail.com>
2015-08-11 17:11:54 +0300

    added new field:

--- users/models.py
+++ users/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
     age = models.IntegerField()
     idnp = models.CharField(max_length=13)
     ex_client = models.BooleanField(default=False)
+
