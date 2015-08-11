018d243ad401c4d0ab766b2f65e868aed1840fa2
dimmg <dumitru.gira@gmail.com>
2015-08-11 17:14:21 +0300

    address field

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=50)
-    address = models.CharField(max_length=100)
+    address = models.CharField(max_length=100, blank=True)
     age = models.IntegerField()
     idnp = models.CharField(max_length=13)
     ex_client = models.BooleanField(default=False)
