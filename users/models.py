3e7f851e8a39d5267c75f5620a476fde61b2e8f1
dimmg <dumitru.gira@gmail.com>
2015-08-11 17:52:23 +0300

    ..

--- users/models.py
+++ users/models.py
@@ -5,5 +5,5 @@ class User(models.Model):
     name = models.CharField(max_length=50)
     age = models.IntegerField()
     idnp = models.CharField(max_length=13)
-    ex_client = models.BooleanField(default=False)
+    was_client = models.BooleanField(default=False)
 
