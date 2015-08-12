1460bcd49db81207219f0c3ec078abf9c7755e00
dimmg <dumitru.gira@gmail.com>
2015-08-12 15:16:55 +0300

    ...

--- users/models.py
+++ users/models.py
@@ -2,12 +2,6 @@ from django.db import models
 
 
 class User(models.Model):
-    GENDER = (
-        ('-', 'unspecified'),
-        ('M', 'male'),
-        ('F', 'female')
-    )
     name = models.CharField(max_length=50)
     idnp = models.CharField(max_length=13)
-    gender = models.CharField(max_length=1, choices=GENDER, default='-') 
 
--- users/serializers.py
+++ users/serializers.py
@@ -6,4 +6,4 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('name', 'idnp', 'gender')
+        fields = ('name', 'idnp')
