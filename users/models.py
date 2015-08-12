6ac7bebe53d6bb9ff12325a882e0388af4b9e78f
dimmg <dumitru.gira@gmail.com>
2015-08-12 15:26:55 +0300

    ...

--- users/models.py
+++ users/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=50)
     idnp = models.CharField(max_length=13)
-
+    ex_client = models.BooleanField(default=False)
--- users/serializers.py
+++ users/serializers.py
@@ -6,4 +6,4 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('name', 'idnp')
+        fields = ('name', 'idnp', 'ex_client')
