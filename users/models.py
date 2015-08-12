0f979a02b86fbc47c53c397161da322d09505970
dimmg <dumitru.gira@gmail.com>
2015-08-12 14:53:29 +0300

    ...

--- users/models.py
+++ users/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 
 class User(models.Model):
+    GENDER = (
+        ('M', 'male'),
+        ('F', 'female')
+    )
     name = models.CharField(max_length=50)
     age = models.IntegerField()
     idnp = models.CharField(max_length=13)
-    x_client = models.BooleanField(default=False)
+    gender = models.CharField(max_length=1, choices=GENDER) 
 
