773fa1a07072de47e88aa399e833cbfca18ade66
dimmg <dumitru.gira@gmail.com>
2015-08-13 12:23:09 +0300

    v2

--- users/migrations/0001_initial.py
+++ users/migrations/0001_initial.py
@@ -15,8 +15,8 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=50)),
-                ('age', models.IntegerField()),
                 ('idnp', models.CharField(max_length=13)),
+                ('ex_client', models.BooleanField(default=False)),
             ],
         ),
     ]
